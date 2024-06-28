d4b0cbd633d71e121aaecc6154a8fa169c960a97
Krzysztof Szczygieł <kszczygiel@whitelabelcoders.com>
2024-06-28 05:33:12 +0200

    feat: create project remove command and update deno version

--- src/commands/project/remove/remove.ts
+++ src/commands/project/remove/remove.ts
@@ -1,41 +1,50 @@
 import { TCommandArgs, TCommandMeta } from '../../../classes/command/command.d.ts';
 import { classCommand } from '../../../classes/command/command.ts';
-import { CLI_PROJECT_NAME_PREFIX } from '../../../constants/CLI_PROJECT_NAME_PREFIX.ts';
-import { CLI_PROJECT_STRUCTURE } from '../../../constants/CLI_PROJECT_STRUCTURE.ts';
 import { logger } from '../../../global/logger.ts';
-import createProjectStructure from '../../../utils/create_project_structure/create_project_structure.ts';
-import { cwd } from '../../../utils/cwd/cwd.ts';
-import { generateUniqueBasename } from '../../../utils/generate_unique_basename/generate_unique_basename.ts';
-import { pathExist } from '../../../utils/path_exist/path_exist.ts';
 import { commandProjectRemoveDocs } from './remove.docs.ts';
-import { pwd } from '../../../utils/pwd/pwd.ts';
+import { getDirName, pwd } from '../../../utils/pwd/pwd.ts';
+import { cwd } from '../../../utils/cwd/cwd.ts';
 
 const phrase = 'project remove';
 
 class classCommandProjectRemove extends classCommand {
   constructor(args: TCommandArgs) {
-    logger.debugFn(arguments);
-
     super(args);
+    logger.debugFn(arguments);
   }
 
   public async exec() {
     logger.debugFn(arguments);
-    const data = await this.getInputData();
-  }
 
-  public async getInputData() {
-    return {
-      projectName: this.getOrAskForArg(
-        'force',
-        'Enter project name:',
-        false,
-        await generateUniqueBasename({
-          basePath: cwd(),
-          prefix: `${CLI_PROJECT_NAME_PREFIX}-`,
-        }),
-      ),
-    };
+    const currentPwd = await pwd();
+
+    if (!currentPwd) {
+      logger.info('Operation cancelled. Not a WPD directory!');
+      return;
+    }
+
+    const projectName = getDirName(currentPwd);
+
+    const forceRemove = this.args.boolean.includes('f') || this.args.boolean.includes('force');
+
+    let userInput = '';
+
+    if (!forceRemove) {
+      const confirmationMessage =
+        `Are you sure you want to remove the project "${projectName}"? This action cannot be undone. Please type the project name: `;
+      userInput = await this.askForArg(confirmationMessage, false, projectName);
+
+      if (userInput !== projectName) {
+        logger.info('Operation cancelled, wrong project name!');
+        return;
+      }
+    }
+
+    Deno.chdir(`${cwd()}/../`);
+
+    await Deno.remove(projectName, { recursive: true });
+
+    logger.info(`Project "${projectName}" has been removed.`);
   }
 }
 
