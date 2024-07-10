7ddaea94b68eacfbd91e97ca7591b64ef0899bf5
Krzysztof Szczygieł <kszczygiel@whitelabelcoders.com>
2024-07-10 10:27:36 +0200

    feat: include env-add command

--- src/commands/index/index.docs.ts
+++ src/commands/index/index.docs.ts
@@ -2,12 +2,15 @@ import { DOCUMENTATION_COLOR_THEME } from '../../constants/DOCUMENTATION_COLOR_T
 import { generateDocumentation } from '../../utils/generate_documentation.ts';
 import commandProjectInitMeta from '../project/init/init.ts';
 import commandProjectRemoveMeta from '../project/remove/remove.ts';
+import commandProjectEnvAddMeta from '../project/env-add/env-add.ts';
 import { description as commandProjectInitDescription } from '../project/init/init.docs.ts';
 import { description as commandProjectRemoveDescription } from '../project/remove/remove.docs.ts';
+import { description as commandProjectEnvAddDescription } from '../project/env-add/env-add.docs.ts';
 
 const feedCommands: [string, string][] = [
 	[commandProjectInitMeta.phrase, commandProjectInitDescription],
 	[commandProjectRemoveMeta.phrase, commandProjectRemoveDescription],
+	[commandProjectEnvAddMeta.phrase, commandProjectEnvAddDescription],
 ];
 
 const feedOptions: [string | string[], string][] = [
